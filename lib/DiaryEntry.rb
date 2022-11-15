8b2d28eb0c3d46039c9709e6c13abecaf7098dee
mahaque <haquema@outlook.com>
2022-11-15 13:49:29 +0000

    add tests for looping over reading chunks

--- lib/DiaryEntry.rb
+++ lib/DiaryEntry.rb
@@ -1,47 +1,28 @@
 class DiaryEntry
-  def initialize(title, contents) # title, contents are strings
-    # ...
+  def initialize(title, contents)
     @title = title
     @contents = contents
     @counter = 0
   end
 
   def title
-    # Returns the title as a string
     @title
   end
 
   def contents
-    # Returns the contents as a string
     @contents
   end
 
   def count_words
-    # Returns the number of words in the contents as an integer
     @contents.split.count
   end
 
-  def reading_time(wpm) # wpm is an integer representing the number of words the
-                        # user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
-    word_count = @contents.split.length
-      if word_count != 0 
-        time_to_read = (word_count.to_f/ wpm).ceil
-      else
-        0
-    end
+  def reading_time(wpm)
+    fail "reading speed cannot be 0" unless wpm.positive?
+    (count_words / wpm.to_f).ceil
   end
 
-  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
-                                  # of words the user can read per minute
-                                  # `minutes` is an integer representing the
-                                  # number of minutes the user has to read
-    # Returns a string with a chunk of the contents that the user could read
-    # in the given number of minutes.
-    # If called again, `reading_chunk` should return the next chunk, skipping
-    # what has already been read, until the contents is fully read.
-    # The next call after that it should restart from the beginning.
+  def reading_chunk(wpm, minutes)
     num_words = wpm * minutes
     words = @contents.split
     chunks = words.each_slice(num_words).to_a
--- spec/DiaryEntry_spec.rb
+++ spec/DiaryEntry_spec.rb
@@ -6,52 +6,72 @@ RSpec.describe DiaryEntry do
     expect(entry.title).to eq "Monday"
     expect(entry.contents).to eq "It rained"
   end
-  it "counts number of words in contents" do
-    entry = DiaryEntry.new("Monday", "It rained")
-    num_words = entry.count_words
-    expect(num_words).to eq 2
-  end
-  it "returns the estimated reading time for the contents when given the reading speed" do
-    entry = DiaryEntry.new("Monday", "word " * 400)
-    result = entry.reading_time(200)
-    expect(result).to eq 2
-  end
-  context "given a reading rate and set number of minutes, where the text is longer than the user can read" do
-    it "returns the chunk of contents the user can read" do
-      entry = DiaryEntry.new("Monday", "one two three")
-      result = entry.reading_chunk(2, 1)
-      expect(result).to eq "one two"
+  
+  describe "#count_words method" do
+    it "correctly counts number of words in contents" do
+      entry = DiaryEntry.new("Monday", "It rained")
+      expect(entry.count_words).to eq 2
     end
-    it "returns the next chunk of text if text is still longer than the user can read" do
-      entry = DiaryEntry.new("Monday", "one two three four five")
-      entry.reading_chunk(2, 1)
-      result = entry.reading_chunk(2, 1)
-      expect(result).to eq "three four"
-    end
-    it "returns the next chunk of text if text is still longer than the user can read" do
-      entry = DiaryEntry.new("Monday", "one two three four five")
-      entry.reading_chunk(2, 1)
-      entry.reading_chunk(2, 1)
-      result = entry.reading_chunk(2, 1)
-      expect(result).to eq "five"
-    end
-    it "returns the next chunk of text if text is still longer than the user can read" do
-      entry = DiaryEntry.new("Monday", "one two three four five")
-      entry.reading_chunk(2, 1)
-      entry.reading_chunk(2, 1)
-      entry.reading_chunk(2, 1)
-      result = entry.reading_chunk(2, 1)
-      expect(result).to eq "one two"
-    end
-    it "returns the next chunk of text if text is still longer than the user can read" do
-      entry = DiaryEntry.new("Monday", "one two three four five")
-      entry.reading_chunk(2, 1)
-      entry.reading_chunk(2, 1)
-      entry.reading_chunk(2, 1)
-      entry.reading_chunk(2, 1)
-      result = entry.reading_chunk(2, 1)
-      expect(result).to eq "three four"
+
+    it "returns a 0 when empty string is passed as contents" do
+      entry = DiaryEntry.new("Monday", "")
+      expect(entry.count_words).to eq 0
     end
   end
 
+  describe "#reading_time method" do
+    context "given a positive, non zero wpm" do
+      it "returns the correct reading time as an integer" do
+        entry = DiaryEntry.new("Monday", "word " * 400)
+        expect(entry.reading_time(200)).to eq 2
+      end
+    end
+    
+    context "given a zero value wpm" do
+      it "fails" do
+        entry = DiaryEntry.new("Monday", "Once upon a time...")
+        expect { entry.reading_time(0) }.to raise_error "reading speed cannot be 0"
+      end
+    end
+  end
+
+  describe "#reading_chunk method" do
+    context "given a reading rate and set number of minutes, where the text is longer than the user can read" do
+      it "returns the chunk of contents the user can read" do
+        entry = DiaryEntry.new("Monday", "one two three")
+        result = entry.reading_chunk(2, 1)
+        expect(result).to eq "one two"
+      end
+      it "returns the next chunk of text if text is still longer than the user can read" do
+        entry = DiaryEntry.new("Monday", "one two three four five")
+        entry.reading_chunk(2, 1)
+        result = entry.reading_chunk(2, 1)
+        expect(result).to eq "three four"
+      end
+      it "returns the next chunk of text if text is still longer than the user can read" do
+        entry = DiaryEntry.new("Monday", "one two three four five")
+        entry.reading_chunk(2, 1)
+        entry.reading_chunk(2, 1)
+        result = entry.reading_chunk(2, 1)
+        expect(result).to eq "five"
+      end
+      it "returns the next chunk of text if text is still longer than the user can read" do
+        entry = DiaryEntry.new("Monday", "one two three four five")
+        entry.reading_chunk(2, 1)
+        entry.reading_chunk(2, 1)
+        entry.reading_chunk(2, 1)
+        result = entry.reading_chunk(2, 1)
+        expect(result).to eq "one two"
+      end
+      it "returns the next chunk of text if text is still longer than the user can read" do
+        entry = DiaryEntry.new("Monday", "one two three four five")
+        entry.reading_chunk(2, 1)
+        entry.reading_chunk(2, 1)
+        entry.reading_chunk(2, 1)
+        entry.reading_chunk(2, 1)
+        result = entry.reading_chunk(2, 1)
+        expect(result).to eq "three four"
+      end
+    end
+  end
 end
